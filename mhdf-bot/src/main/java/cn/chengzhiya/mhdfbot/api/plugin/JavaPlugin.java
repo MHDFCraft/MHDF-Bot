b26fdc89dc90bf26891aee4b9d9830b08d1255b5
白神遥真可爱 <292200693@qq.com>
2024-12-03 12:54:37 +0800

    实现插件模块的注册

--- mhdf-bot/src/main/java/cn/chengzhiya/mhdfbot/api/plugin/JavaPlugin.java
+++ mhdf-bot/src/main/java/cn/chengzhiya/mhdfbot/api/plugin/JavaPlugin.java
@@ -1,7 +1,9 @@
 package cn.chengzhiya.mhdfbot.api.plugin;
 
 import cn.chengzhiya.mhdfbot.api.MHDFBot;
+import cn.chengzhiya.mhdfbot.api.entity.plugin.Command;
 import cn.chengzhiya.mhdfbot.api.entity.plugin.PluginInfo;
+import cn.chengzhiya.mhdfbot.api.listener.Listener;
 import lombok.Data;
 import org.apache.logging.log4j.Logger;
 
@@ -10,7 +12,31 @@ import org.apache.logging.log4j.Logger;
 public abstract class JavaPlugin implements Plugin {
     private PluginInfo pluginInfo;
 
+    /**
+     * 获取日志实例
+     *
+     * @return 日志实例
+     */
     public Logger getLogger() {
-        return MHDFBot.getLogger(pluginInfo.getName());
+        return MHDFBot.getLogger(this.pluginInfo.getName());
+    }
+
+    /**
+     * 注册命令实例
+     *
+     * @param command 命令实例
+     */
+    public void registerCommand(Command command) {
+        command.plugin(this.pluginInfo);
+        MHDFBot.getCommandManager().registerCommand(command);
+    }
+
+    /**
+     * 注册指定监听器实例
+     *
+     * @param listener 监听器实例
+     */
+    public void registerListener(Listener listener) {
+        MHDFBot.getListenerManager().registerListener(this.pluginInfo, listener);
     }
 }
