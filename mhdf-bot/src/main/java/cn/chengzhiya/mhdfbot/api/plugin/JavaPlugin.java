d9fa8f93d00fe8703df9f4bf9e314421de5598eb
白神遥真可爱 <292200693@qq.com>
2024-12-03 13:17:01 +0800

    实现资源操作

--- mhdf-bot/src/main/java/cn/chengzhiya/mhdfbot/api/plugin/JavaPlugin.java
+++ mhdf-bot/src/main/java/cn/chengzhiya/mhdfbot/api/plugin/JavaPlugin.java
@@ -1,16 +1,21 @@
 package cn.chengzhiya.mhdfbot.api.plugin;
 
 import cn.chengzhiya.mhdfbot.api.MHDFBot;
+import cn.chengzhiya.mhdfbot.api.entity.config.YamlConfiguration;
 import cn.chengzhiya.mhdfbot.api.entity.plugin.Command;
 import cn.chengzhiya.mhdfbot.api.entity.plugin.PluginInfo;
 import cn.chengzhiya.mhdfbot.api.listener.Listener;
+import cn.chengzhiya.mhdfbot.api.util.FileUtil;
 import lombok.Data;
 import org.apache.logging.log4j.Logger;
+
+import java.io.File;
 
 @Data
 @SuppressWarnings("unused")
 public abstract class JavaPlugin implements Plugin {
     private PluginInfo pluginInfo;
+    private YamlConfiguration config;
 
     /**
      * 获取日志实例
@@ -38,5 +43,39 @@ public abstract class JavaPlugin implements Plugin {
      */
     public void registerListener(Listener listener) {
         MHDFBot.getListenerManager().registerListener(this.pluginInfo, listener);
+    }
+
+    /**
+     * 保存默认配置文件
+     */
+    public void saveDefaultConfig() {
+        this.saveResource("config.yml", "config.yml", false);
+    }
+
+    /**
+     * 重载配置文件
+     */
+    public void reloadConfig() {
+        this.config = YamlConfiguration.loadConfiguration(new File(this.getDataFolder(), "config.yml"));
+    }
+
+    /**
+     * 保存资源
+     *
+     * @param filePath     保存目录
+     * @param resourcePath 资源目录
+     * @param replace      替换文件
+     */
+    public void saveResource(String filePath, String resourcePath, boolean replace) {
+        FileUtil.saveResource(new File(this.getDataFolder(),filePath).getPath(), resourcePath, replace);
+    }
+
+    /**
+     * 获取插件数据目录实例
+     *
+     * @return 数据目录实例
+     */
+    public File getDataFolder() {
+        return new File("./plugins/" + this.pluginInfo.getName());
     }
 }
